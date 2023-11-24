2e102c84bec4d4e9ebbc8b785660f24645595f12
stranger-creator <raosuhas0@gmail.com>
2023-11-24 10:16:18 +0530

    Navbar Done

--- disney/src/App.js
+++ disney/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Login from './components/Login'
+import Header from './components/Header'
 import {Route,Routes, BrowserRouter} from "react-router-dom"
 function App() {
   return (
     <div className="App">
     <BrowserRouter>
+    <Header/>
       <Routes>
       <Route path="/"element={<Login />}></Route>
       </Routes>
